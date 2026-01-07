28edc2b17440cb737bb710268ac232bed4d2f826
yws <dbdntjs620@gmail.com>
2026-01-07 16:18:30 +0900

    feat : Application.kt
    - OncallContoller.kt 호출

--- src/main/kotlin/oncall/Application.kt
+++ src/main/kotlin/oncall/Application.kt
@@ -1,5 +1,17 @@
 package oncall
 
+import oncall.controller.OncallController
+import oncall.util.Extractor
+import oncall.util.Validator
+import oncall.view.InputView
+import oncall.view.OutputView
+
+
 fun main() {
-    TODO("프로그램 구현")
+    // TODO("프로그램 구현")
+    val inputView = InputView()
+    val outputView = OutputView()
+    val validator = Validator()
+    val extractor = Extractor()
+    OncallController(inputView, outputView,validator, extractor).run()
 }
