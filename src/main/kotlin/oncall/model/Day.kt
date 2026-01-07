7dd599ba2e808e9dae7203c1b32e07009a5d5a93
yws <dbdntjs620@gmail.com>
2026-01-07 16:12:16 +0900

    feat : Day.kt
    각 일자의 요일과 평일, 주말, 휴일을 구분하기 위한 필드 정의
    각 필드에 접근하기위한 메소드 구현

---
+++ src/main/kotlin/oncall/model/Day.kt
@@ -0,0 +1,33 @@
+package oncall.model
+
+import oncall.constant.Holiday
+
+class Day(
+    private val month: Int,
+    private val date: Int,
+    private val day: String,
+    private val isHoliday: Boolean,
+    private val isWeekend: Boolean
+) {
+    private var nickName: String = ""
+
+    fun printInfo() {
+        println("${month}, ${date}, ${day}, ${isHoliday}, ${isWeekend}")
+    }
+
+    fun setNickName(input: String) {
+        this.nickName = input
+    }
+
+    fun getNickName(): String { return this.nickName }
+
+    fun getMonth(): Int { return this.month }
+
+    fun getDate(): Int { return this.date }
+
+    fun getDay(): String { return this.day }
+
+    fun isHoliday(): Boolean { return this.isHoliday }
+
+    fun isWeekend(): Boolean { return this.isWeekend }
+}
